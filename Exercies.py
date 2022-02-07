3d67106f5aa8fda9c63552a0f1a4a55e36198ae9
Kingsly Selvaratnam <kingslyselvaratnam@improbable.io>
2022-02-07 15:22:16 +0000

    added exercise one code

---
+++ Exercies.py
@@ -0,0 +1,12 @@
+mport machine
+import utime
+
+
+#setup the onboard led pin as an output
+LED = machine.Pin(25,machine.Pin.OUT)
+
+while True:
+    LED.value(1)
+    utime.sleep_ms(500)
+    LED.value(0)
+    utime.sleep_ms(500)
